f82f4f6db54d3c3dd4ceaf6dec50b4e9c7dbe479
Dan Ober <danober.dev@gmail.com>
2023-09-18 22:41:57 -0500

    tested with additional note

--- my-synthesizer-app/src/Synthesizer.js
+++ my-synthesizer-app/src/Synthesizer.js
@@ -3,25 +3,46 @@ import * as Tone from 'tone';
 
 function Synthesizer() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const synth = new Tone.Synth().toDestination();
+  const synth = new Tone.Synth().toDestination(); //create a synth and connect it to the main output
+  const now = Tone.now()
 
-  const playNote = () => {
-    synth.triggerAttackRelease('C4', '4n');
+  const playC4 = () => {
+    synth.triggerAttackRelease('C4', "8n"); // trigger the attack into the note subdivision (i.e. 8n = 8th note) or immediately ("now")
+    // synth.triggerRelease(now + 1) // wait one second before triggering the release
   };
 
-  const togglePlay = () => {
+  const playG4 = () => {
+    synth.triggerAttackRelease('G4', "8n"); // trigger the attack into the note subdivision (i.e. 8n = 8th note) or immediately ("now")
+    // synth.triggerRelease(now + 1) // wait one second before triggering the release
+  };
+
+  const toggleC4 = () => {
     if (isPlaying) {
       synth.triggerRelease();
     } else {
-      playNote();
+      playC4();
     }
     setIsPlaying(!isPlaying);
   };
 
+  const toggleG4 = () => {
+    if (isPlaying) {
+      synth.triggerRelease();
+    } else {
+      playG4();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+
   return (
     <div>
-      <h1>Synth Tone in C4</h1>
-      <button onClick={togglePlay}>
+      <h1>C4</h1>
+      <button onClick={toggleC4}>
+        {isPlaying ? 'Stop' : 'Play'}
+      </button>
+      <h1>G4</h1>
+      <button onClick={toggleG4}>
         {isPlaying ? 'Stop' : 'Play'}
       </button>
     </div>
